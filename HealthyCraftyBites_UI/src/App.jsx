421e3b02705aa0399d1d363dd9850b3b19fd8656
Atharv Sabale <atharvsabale7040@gmail.com>
2026-07-30 00:26:16 +0530

    Added home page UI and admin login navigation

--- HealthyCraftyBites_UI/src/App.jsx
+++ HealthyCraftyBites_UI/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./layout_pages/home_page/Home";
 import AdminManagerHome from "./layout_pages/admin_manager/AdminManagerHome";
 import AdminLogin from "./services/authentication_service/pages_authentication_service/AdminLogin"
 import AddProductForm from "./services/menu_management_service/component_menu_management_service/AddProductForm"
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/adminlogin" element={<AdminLogin />} />
                <Route path="/adminmanagerhome" element={<AdminManagerHome />}>
                   <Route path="addproduct" element={<AddProductForm />} />
